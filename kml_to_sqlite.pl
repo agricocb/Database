37b00b530ad6fe493112990c52e5a8c105677d75
Craig B. Agricola <craig@theagricolas.org>
2014-11-22 17:14:38 -0500

    Collapsed the difficulty and uses into a single table, but differentiated them in the trails table by the summer and winter values.  Also, rather than populating the uses manually in the schema, I auto-add the uses to the uses table the first time that they are encountered.

--- kml_to_sqlite.pl
+++ kml_to_sqlite.pl
@@ -29,31 +29,29 @@ printf("Done.\n");
 printf("Caching difficulty and uses ID values... ");
 my %difficulty;
 my %uses;
-my $diff_query = "select id,english_difficulty from trail_difficulty;";
 my $uses_query = "select id,english_uses from trail_uses;";
-foreach my $diff (@{$dbh->selectall_arrayref($diff_query)}) {
-  $difficulty{$diff->[1]} = $diff->[0];
-}
 foreach my $uses (@{$dbh->selectall_arrayref($uses_query)}) {
   $uses{$uses->[1]} = $uses->[0];
 }
 printf("Done.\n");
 
+my $ins_uses_sth =
+    $dbh->prepare("insert into trail_uses (english_uses) values (?)");
 my $ins_map_obj_sth =
     $dbh->prepare("insert into map_object (id,name) values (?,?)");
 my $ins_trail_sth =
-    $dbh->prepare("insert into trail (id,difficulty_id,uses_id,meters) " .
-                    "values (?,?,?,?)");
+    $dbh->prepare("insert into trail (id,summer_uses_id,winter_uses_id,meters)".
+                    " values (?,?,?,?)");
 my $ins_coords_sth =
     $dbh->prepare("insert into coordinate (trail_id,seq,longitude,lattitude) " .
                     "values (?,?,?,?)");
 
 printf("Parsing trails from XML and inserting into database...\n");
-my %fn = ( difficulty => "TRAILTYPES",
-           uses       => "TRAILTYPEW",
-           name       => "TRAILNAMES",
-           meters     => "METERS",
-           id         => "TMPID");
+my %fn = ( summer_type => "TRAILTYPES",
+           winter_type => "TRAILTYPEW",
+           name        => "TRAILNAMES",
+           meters      => "METERS",
+           id          => "TMPID");
 my $k=new XML::XPath(filename=>$kmlfile);
 my $inserts = 0;
 $dbh->{AutoCommit} = 0;
@@ -68,12 +66,23 @@ foreach my $pm ($k->findnodes("/kml/Document/Folder/Placemark")) {
   $co=[map([split(/,/,$_)],split(/\s+/,$co))];
   $pmd->{coords}=$co;
   printf("Inserting trail ID %d\n", $pmd->{id});
+  if (!defined($uses{$pmd->{summer_type}})) {
+    $ins_uses_sth->execute($pmd->{summer_type});
+    $uses{$pmd->{summer_type}} =
+      $dbh->last_insert_id(undef, undef, undef, undef);
+  }
+  if (!defined($uses{$pmd->{winter_type}})) {
+    $ins_uses_sth->execute($pmd->{winter_type});
+    $uses{$pmd->{winter_type}} =
+      $dbh->last_insert_id(undef, undef, undef, undef);
+  }
   $ins_map_obj_sth->execute($pmd->{id}, $pmd->{name});
-  $ins_trail_sth->execute($pmd->{id}, $difficulty{$pmd->{difficulty}},
-                          $uses{$pmd->{uses}}, $pmd->{meters});
+  $ins_trail_sth->execute($pmd->{id}, $uses{$pmd->{summer_type}},
+                          $uses{$pmd->{winter_type}}, $pmd->{meters});
   my $seq=1;
   foreach my $waypoint (@$co) {
-    $ins_coords_sth->execute($pmd->{id}, $seq++, $waypoint->[0], $waypoint->[1]);
+    $ins_coords_sth->execute($pmd->{id}, $seq++,
+                             $waypoint->[0], $waypoint->[1]);
   }
   if (++$inserts%100==0) { $dbh->commit(); }
 }
@@ -83,16 +92,6 @@ $dbh->{AutoCommit} = 1;
 
 __DATA__
 
-create table trail_difficulty (
-  id                  integer     primary key autoincrement,
-  english_difficulty  varchar
-);
-
-create table trail_uses (
-  id                  integer     primary key autoincrement,
-  english_uses        varchar
-);
-
 create table map_object (
   id                  integer primary key autoincrement,
   name                varchar,
@@ -100,14 +99,19 @@ create table map_object (
   url                 varchar
 );
 
+create table trail_uses (
+  id                  integer     primary key autoincrement,
+  english_uses        varchar
+);
+
 create table trail (
   id                  int,
-  difficulty_id       int,
-  uses_id             int,
+  summer_uses_id      int,
+  winter_uses_id      int,
   meters              double,
-  foreign key (id)            references map_object       (id),
-  foreign key (difficulty_id) references trail_difficulty (id),
-  foreign key (uses_id)       references trail_uses       (id)
+  foreign key (id)              references map_object       (id),
+  foreign key (summer_uses_id)  references trail_uses       (id),
+  foreign key (winter_uses_id)  references trail_uses       (id)
 );
 
 create table coordinate (
@@ -117,23 +121,3 @@ create table coordinate (
   longitude           double,
   foreign key (trail_id) references trails(id)
 );
-
-insert into trail_difficulty (english_difficulty) values ('BikePath');
-insert into trail_difficulty (english_difficulty) values ('Easy');
-insert into trail_difficulty (english_difficulty) values ('Extreme');
-insert into trail_difficulty (english_difficulty) values ('Hard');
-insert into trail_difficulty (english_difficulty) values ('Moderate');
-insert into trail_difficulty (english_difficulty) values ('Not');
-insert into trail_difficulty (english_difficulty) values ('PvtRd');
-insert into trail_difficulty (english_difficulty) values ('Skip');
-insert into trail_difficulty (english_difficulty) values ('Walking');
-
-insert into trail_uses (english_uses) values ('Motor');
-insert into trail_uses (english_uses) values ('MotorSkiShoe');
-insert into trail_uses (english_uses) values ('Not');
-insert into trail_uses (english_uses) values ('PvtRd');
-insert into trail_uses (english_uses) values ('Shoe');
-insert into trail_uses (english_uses) values ('Ski');
-insert into trail_uses (english_uses) values ('Skip');
-insert into trail_uses (english_uses) values ('SkiShoe');
-insert into trail_uses (english_uses) values ('Unmaintained');
